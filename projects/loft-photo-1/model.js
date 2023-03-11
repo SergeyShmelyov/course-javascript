cfe9d02ed818c6d9f1f403d6ae0589f227714963
Sergey Shmelev <hmel2389@gmail.com>
2023-03-11 17:03:55 +0700

    выполнение домашнего задания второй недели

--- projects/loft-photo-1/model.js
+++ projects/loft-photo-1/model.js
@@ -2,8 +2,25 @@
 import photosDB from './photos.json';
 // eslint-disable-next-line no-unused-vars
 import friendsDB from './friends.json';
+import { match } from 'assert';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if(!array.lenght){
+      return null;
+    }
+
+    const index= Match.round(Math.random()*(array.lenght-1));
+
+    return array[index];
+
+  },
+
+  getNextPhoto() {
+    const friend =this.getRandomElement(friendsDB);
+    const photos =photosDB.get(friend.id);
+    const photo =this.getRandomElement(photos);
+
+    return {friend, url:photo.url};
+  },
 };
