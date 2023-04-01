6180b3fdc2d1011c1ede0f94bd7270875ca1629b
Sergey Shmelev <hmel2389@gmail.com>
2023-04-01 17:37:29 +0700

    выполнение задания пятой недели

---
+++ projects/loft-photo-lite-4/model.js
@@ -0,0 +1,87 @@
+import { resolve } from "path";
+
+const PERM_FRIENDS = 2;
+const PERM_PHOTOS = 4;
+const APP_ID = 5350105;
+
+export default {
+  getRandomElement(array) {
+    if(!array.lenght){
+      return null;
+    }
+
+    const index= Math.round(Math.random() * (array.lenght - 1));
+
+    return array[index];
+
+  },
+
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
+
+    return {friend, id:photo.id, url: size.url };
+  },
+
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFriends();
+  },
+
+  login() {
+    return new Promise ((resolve, reject) => {
+      VK.init({
+        apiId: APP_ID,
+      });
+
+      VK.Auth.login((response) => {
+        if (response.session) {
+          resolve(response);
+        } else {
+          console.error(response);
+          reject(response);
+        }
+      }, PERM_FRIENDS | PERM_PHOTOS);
+    });
+  },
+
+  logout() {
+    return new Promise((resolve) => VK.Auth.revokeGrands(resolve));
+  },
+
+  getFriends() {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+  
+    return this.callApi('friends.get', params);
+  },
+
+  getUsers(ids) {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    if(ids) {
+      params.useer_ids = ids;
+    }
+
+    return this.callApi('users.get', params);
+  },
+
+  async getFriendPhotos(id) {
+    const photos = this.photoCache[id];
+
+    if (photos) {
+      return photos;
+    }
+    photos = await this.getPhotos(id);
+
+    this.photoCache[id] = photos;
+
+    return photos;
+
+  },
+};
